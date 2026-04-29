c236624a46ed3f8ff05d0008d74ab3b7d46b3b0a
Lavanya <lavanya3218119@gmail.com>
2026-04-30 00:37:14 +0530

    force static fix

--- myproject/settings.py
+++ myproject/settings.py
@@ -137,7 +137,7 @@ LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'login'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 import dj_database_url
 
