2e7790421cac113e45a9c54c8566e375e6125c0f
Lavanya <lavanya3218119@gmail.com>
2026-05-01 13:39:16 +0530

    fix redirect path

--- demo/urls.py
+++ demo/urls.py
@@ -3,6 +3,6 @@ from .views import temp
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='auth/login/', permanent=False), name='home'),
+    path('', RedirectView.as_view(url='/auth/login/', permanent=False), name='home'),
     path('temp/', temp, name='temp'),
 ]
